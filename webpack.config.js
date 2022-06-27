090b0063396c49c68477627e583026a2bbb46ba0
limengting <1443848358@qq.com>
2022-06-27 20:34:43 +0800

    完成了插件-自动清除dist目录内容，设置mode为开发模式

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@
 const path = require('path')
 //  引入插件-自动生成html文件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+//  引入插件-自动清除dist目录内容
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
+    mode:'development',//开发阶段模式，简易打包，打包速度快
     entry:'./src/index.js',
     output:{
         path:path.resolve(__dirname,'dist'),// 出口路径 绝对路径
@@ -13,7 +16,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template:'./public/index.html',// 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
             filename:'index.html'// 生成文件的名称
-        })
+        }),
+        new CleanWebpackPlugin()
     ]
     
 }
