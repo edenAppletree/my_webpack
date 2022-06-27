deac88d286499ce1edaef0838ba028fea920b00d
limengting <1443848358@qq.com>
2022-06-27 20:55:21 +0800

    完成了加载器 - 处理less文件配置

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './tab'
 
 // 2.引入jquery
 import $ from 'jquery'
-$('#swiper').css('background-color','pink')
+$('#swiper').css('background-color','yellowgreen')
 
 // 3.引入css样式  加载器 - 处理css文件
 import './styles/index.css'
 
+// 4.引入less样式  加载器 - 处理less文件
+import './styles/index.less'
--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,37 @@ const path = require('path')
 //  引入插件-自动生成html文件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 //  引入插件-自动清除dist目录内容
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 
 module.exports = {
-    mode:'development',//开发阶段模式，简易打包，打包速度快
-    entry:'./src/index.js',
-    output:{
-        path:path.resolve(__dirname,'dist'),// 出口路径 绝对路径
-        filename:'bundle.js'// 出口文件名
+    mode: 'development',//开发阶段模式，简易打包，打包速度快
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),// 出口路径 绝对路径
+        filename: 'bundle.js'// 出口文件名
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:'./public/index.html',// 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
-            filename:'index.html'// 生成文件的名称
+            template: './public/index.html',// 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
+            filename: 'index.html'// 生成文件的名称
         }),
         new CleanWebpackPlugin()
     ],
     devServer: {
-        port:3000,//端口号，下面设置运行时自动打开
-        open:true
+        port: 3000,//端口号，下面设置运行时自动打开
+        open: true
     },
     module: {
         rules: [{
-            test:/\.css$/, // 匹配所有的css文件
-            use:['style-loader','css-loader']
-        }]
+            test: /\.css$/, // 匹配所有的css文件
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.less$/, // 匹配执行类型的文件
+            use: ["style-loader", "css-loader", 'less-loader']
+        }
+        ]
     }
-    
+
 }
